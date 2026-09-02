df5385b66e8bf63e0184cb0e64971549b4f65191
Pedro <coutopedro607q@gmail.com>
2026-09-02 10:45:04 -0300

    Register auth blueprint in app.py

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:@localhost:3306/Cafezin"
-
+app.register_blueprint("auth")
 CORS(app)
 
 
