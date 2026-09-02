ae1c83e31f3b1df960a5bd59a20fa4e6d399f60b
Pedro <coutopedro607q@gmail.com>
2026-09-02 10:26:22 -0300

    Update and rename Usuario.py to init.py

---
+++ backend/models/init.py
@@ -0,0 +1,34 @@
+from .usuario import Usuario
+from .comprador import Comprador
+from .endereco import Endereco
+from .comprador_endereco import Comprador_Endereco
+from .pagamento import Pagamento
+from .entregador import Entregador
+from .categoria import Categoria
+from .loja import Loja
+from .produto import Produto
+from .pedido import Pedido
+from .item_pedido import ItemPedido
+from .entrega import Entrega
+from .avaliacao_entregador import AvaliacaoEntregador
+from .avaliacao_loja import AvaliacaoLoja
+from .avaliacao_produto import AvaliacaoProduto
+ 
+__all__ = [
+    "Usuario",
+    "Comprador",
+    "Endereco",
+    "Comprador_Endereco",
+    "Pagamento",
+    "Entregador",
+    "Categoria",
+    "Loja",
+    "Produto",
+    "Pedido",
+    "ItemPedido",
+    "Entrega",
+    "AvaliacaoEntregador",
+    "AvaliacaoLoja",
+    "AvaliacaoProduto",
+]
+ 
