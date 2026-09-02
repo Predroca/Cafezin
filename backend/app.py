6e209fc51eaad490199cc4476a32fb0f1af4d916
Pedro <coutopedro607q@gmail.com>
2026-09-02 10:50:58 -0300

    Fix blueprint registration for auth route

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,11 @@
 from flask import Flask, request
 from flask import 
 from flask_cors import CORS
-
+from routes.auth_rotas import auth_rotas
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:@localhost:3306/Cafezin"
-app.register_blueprint("auth")
+app.register_blueprint(auth)
 CORS(app)
 
 
