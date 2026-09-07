6629cfd1b1c9d001ab4e2eafc80846245dbe3b23
Gui Lobianco <guilhermealveslobianco@gmail.com>
2026-09-07 15:35:25 -0300

    Mudanças nas animações do index

---
+++ HTML/JavaScript/animationScript.js
@@ -0,0 +1,17 @@
+let bord = document.getElementById('bttnClick');
+const listE = document.getElementsByTagName('li');
+const bordPX = bord.getBoundingClientRect().left;
+console.log(bordPX)
+for(let li of listE){
+    li.addEventListener('mouseenter',()=>{
+        console.log(li.innerText);
+        let liRect = li.getBoundingClientRect();
+        let bordRect = bord.getBoundingClientRect();
+        let liCenter = liRect.left + liRect.width / 2;
+        let bordCenter = bordRect.width / 2;
+
+        let alvoX = liCenter-bordCenter-bordPX;
+        bord.style.transform = `translateX(${alvoX}px)`;
+    })
+}
+
