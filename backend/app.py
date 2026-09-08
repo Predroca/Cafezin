1a3419347e897f7a184494024aa39ac15192a665
Felipe.PXB <fe90pe2010@gmail.com>
2026-09-08 10:07:52 -0300

    Register catalogo_bp blueprint in app.py
    Added catalogo_bp blueprint registration to the app.

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,12 @@
 from flask import Flask, request
 from flask import 
 from flask_cors import CORS
-from routes.auth_rotas import auth_rotas
+from routes import auth_rotas , catalogo_bp
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:@localhost:3306/Cafezin"
 app.register_blueprint(auth_bp)
+app.register_blueprint(catalogo_bp)
 CORS(app)
 
 
