2e824b1b3a2ed94be539fa96cf2d681e2cdb5c17
Pedro <coutopedro607q@gmail.com>
2026-09-09 10:43:56 -0300

    Replace auth_bp with auth_rotas in app.py

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ from routes import auth_rotas , catalogo_bp
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:@localhost:3306/Cafezin"
-app.register_blueprint(auth_bp)
+app.register_blueprint(auth_rotas)
 app.register_blueprint(catalogo_bp)
 CORS(app)
 
