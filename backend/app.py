1d5d54fd8790db6a68ba8c43bfc46a46f510dee6
Pedro <coutopedro607q@gmail.com>
2026-09-11 10:58:25 -0300

    Add JWT support and health check endpoint
    Refactor Flask app to include JWT and health check route.

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,26 @@
-from flask import Flask, request
+from flask import Flask
 from flask_cors import CORS
-from routes import auth_rotas , catalogo_bp
+
+from extensions import db, jwt
+from routes.auth_rotas import auth_bp
+from routes.catalogo_bp import catalogo_bp
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:@localhost:3306/Cafezin"
-app.register_blueprint(auth_rotas)
-app.register_blueprint(catalogo_bp)
+app.config["JWT_SECRET_KEY"] = "IcH6IkqK3RgsF964iwmMIWSZ3TpCXZ4JymducTUj5eOQpwmEij1IFdog8dAu7GWj"
+
 CORS(app)
+db.init_app(app)
+jwt.init_app(app)
+
+app.register_blueprint(auth_bp)
+app.register_blueprint(catalogo_bp)
+
+
+@app.route("/health")
+def health():
+    return {"status": "ok"}
 
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
-    
