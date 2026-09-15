2297814afcc3d80cd8411a75e54813bbc224b495
Pedro <coutopedro607q@gmail.com>
2026-09-15 19:54:11 -0300

    Add registration routes for Comprador, Loja, and Entregador

--- backend/routes/auth_rotas.py
+++ backend/routes/auth_rotas.py
@@ -1,16 +1,24 @@
 from flask import Blueprint, request, jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_jwt_extended import create_access_token
-from extensions import db
+
+
 from models.usuario import Usuario
+from models.loja import Loja
+from models.comprador import Comprador
+from models import Entregador
+from models.endereco import Endereco
+from models.pagamento import Pagamento
+
 from flask_cors import CORS
+from extensions import db
 
 auth_bp = Blueprint('auth', __name__)
 
 CORS(auth_bp)
 
-@auth_bp.route('/register', methods=['POST'])
-def registrar():
+@auth_bp.route('/register/comprador', methods=['POST'])
+def registrar_comprador():
     dados = request.get_json()
 
     existe = Usuario.query.filter_by(email=dados['email']).first()
@@ -23,14 +31,120 @@ def registrar():
         email = dados['email'],
         senha = senha_hash,
         nome = dados["nome"],
-        tipo_usuario = dados["tipo_usuario"],
+        tipo_usuario = 'Comprador',
         telefone = dados.get("telefone")
     )
 
     db.session.add(novo_usuario)
+    db.session.flush()
+
+    comprador = Comprador(
+        cpf = dados["cpf"],
+        data_nasc = dados.get('data_nasc'),
+        sexo = dados.get('sexo'),
+        cod_usuario = novo_usuario.cod_usuario
+    )
+
+    db.session.add(comprador)
     db.session.commit()
 
-    return {"menssage" : "usuario criado", "usuario" : novo_usuario.nome}
+    return {"menssage" : "usuario criado", "usuario" : novo_usuario.nome}, 201
+
+
+@auth_bp.route('/register/loja', methods=['POST'])
+def registrar_loja():
+    dados = request.get_json()
+
+    existe = Usuario.query.filter_by(email=dados['email']).first()
+    if existe is not None:
+        return {"error" : "email já cadastrado"}, 409
+    
+    senha_hash = generate_password_hash(dados["senha"])
+
+    novo_usuario = Usuario(
+        email = dados['email'],
+        senha = senha_hash,
+        nome = dados["nome"],
+        tipo_usuario = 'Loja',
+        telefone = dados.get("telefone")
+    )
+
+    db.session.add(novo_usuario)
+    db.session.flush()
+
+    end = dados['endereco']
+    endereco = Endereco(
+        nome_logradouro = end['nome_logradouro'],
+        numero = end.get('numero'),
+        complemento = end.get('complemento'),
+        bairro = end.get('bairro'),
+        cidade = end.get('cidade'),
+        estado = end.get('estado'),
+        cep = end.get('cep')
+    )
+
+    db.session.add(endereco)
+    db.session.flush()
+
+    loja = Loja (
+        cpf = dados["cnpj"],
+        horario_funcionamento=dados.get('horario_funcionamento'),
+        cod_endereco=endereco.cod_endereco,
+        cod_usuario=novo_usuario.cod_usuario
+    )
+
+    db.session.add(loja)
+    db.session.commit()
+
+    return {"menssage" : "loja criada", "usuario" : novo_usuario.nome}, 201
+
+
+@auth_bp.route('/register/entregador', methods=['POST'])
+def registrar_entregador():
+    dados = request.get_json()
+
+    existe = Usuario.query.filter_by(email=dados['email']).first()
+    if existe is not None:
+        return {"error" : "email já cadastrado"}, 409
+    
+    senha_hash = generate_password_hash(dados["senha"])
+
+    novo_usuario = Usuario(
+        email = dados['email'],
+        senha = senha_hash,
+        nome = dados["nome"],
+        tipo_usuario = 'Entregador',
+        telefone = dados.get("telefone")
+    )
+
+    db.session.add(novo_usuario)
+    db.session.flush()
+
+    pag = dados['pagamento']
+    pagamento = Pagamento(
+        tipo = pag['tipo'],
+        data = pag.get('data')
+    )
+
+    db.session.add(pagamento)
+    db.session.flush()
+
+    entregador = Entregador (
+        cpf = dados["cpf"],
+        data_nasc = dados.get('data_nasc'),
+        cnh = dados.get('cnh'),
+        cod_pagamento = pagamento.cod_pagamento,
+        cod_usuario = novo_usuario.cod_usuario
+        
+    )
+
+    db.session.add(entregador)
+    db.session.commit()
+
+    return {"menssage" : "Entregador(a) criado(a)", "usuario" : novo_usuario.nome}, 201
+
+
+
 
 @auth_bp.route('/login', methods=['POST'])
 def login():
