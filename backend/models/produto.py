4c0e2e46a99b5ad31d473b01d91734ff902c512a
Felipe.PXB <fe90pe2010@gmail.com>
2026-09-23 14:12:44 -0300

    Update produto.py

--- backend/models/produto.py
+++ backend/models/produto.py
@@ -16,3 +16,15 @@ class Produto(db.Model):
     loja = db.relationship("Loja", back_populates="produtos")
     categoria = db.relationship("Categoria", back_populates="produtos")
     avaliacoes = db.relationship("AvaliacaoProduto", back_populates="produto")
+
+    def to_dict(self):
+        return {
+            "cod_produto": self.cod_produto,
+            "cod_loja": self.cod_loja,
+            "nome": self.nome,
+            "descricao": self.descricao,
+            "preco": float(self.preco),
+            "disponibilidade": self.disponibilidade,
+            "cod_categoria": self.cod_categoria,
+            "foto_url": self.foto_url
+        }
