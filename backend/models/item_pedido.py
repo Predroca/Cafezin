912a29607f77fc0c13303ce2cf98fa8d8eaea64c
Pedro <coutopedro607q@gmail.com>
2026-09-24 20:58:09 -0300

    Correção do ultimo commit que quebrou o app

--- backend/models/item_pedido.py
+++ backend/models/item_pedido.py
@@ -26,17 +26,4 @@ class ItemPedido(db.Model):
             
             # Puxa automaticamente os dados da classe Produto se o relacionamento existir
             "produto": self.produto.to_dict() if self.produto else None
-        }from extensions import db
-
-
-class ItemPedido(db.Model):
-    __tablename__ = "ItemPedido"
-
-    cod_item = db.Column(db.Integer, primary_key=True)
-    quantidade = db.Column(db.Integer, nullable=False)
-    preco_unitario = db.Column(db.Numeric(10, 2), nullable=False)
-    cod_pedido = db.Column(db.Integer, db.ForeignKey("Pedido.cod_pedido"), nullable=False)
-    cod_produto = db.Column(db.Integer, db.ForeignKey("Produto.cod_produto"), nullable=False)
-
-    pedido = db.relationship("Pedido", back_populates="itens")
-    produto = db.relationship("Produto")
+        }
